d585d520dac0de14c27f40fb3ae83f6670fcc8b2
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-18 01:37:34 -0300

    Modelando DTO Infected

---
+++ ApiMongoDB/Model/InfectedDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiMongoDB.Model
+{
+    public class InfectedDTO
+    {
+        [Required(ErrorMessage = "A data de nascimento é obrigatória!")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "O sexo é obrigatório!")]
+        public string Sexo { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
