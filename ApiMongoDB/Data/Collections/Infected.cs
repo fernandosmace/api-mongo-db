565d17471daaa74004dcf397a709e8cf66cc06d0
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-17 23:24:50 -0300

    Modelando Collection Infected

---
+++ ApiMongoDB/Data/Collections/Infected.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMongoDB.Data.Collections
+{
+    public class Infected
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+
+
+        public Infected(DateTime dataNascimento, string sexo, double longitude, double latitude)
+        {
+            DataNascimento = dataNascimento;
+            Sexo = sexo;
+            Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }
+    }
+}
