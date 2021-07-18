c4dbad9a24ee310cad61997a8e5948509bda4e1e
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-18 14:30:48 -0300

    Adicionado output para InfectedDTO

---
+++ ApiMongoDB/Model/InfectedDTOOutput.cs
@@ -0,0 +1,14 @@
+﻿using ApiMongoDB.Data.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMongoDB.Model
+{
+    public class InfectedDTOOutput
+    {
+        public string Message { get; set; }
+        public Infected Infected { get; set; }
+    }
+}
