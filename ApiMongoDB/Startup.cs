73bc43b3b283fbaa9309f28fd016e1e65eaf06ae
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-18 01:37:25 -0300

    Injetando dependência do MongoDB

--- ApiMongoDB/Startup.cs
+++ ApiMongoDB/Startup.cs
@@ -32,6 +32,7 @@ namespace ApiMongoDB
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiMongoDB", Version = "v1" });
             });
+            services.AddScoped<Data.MongoDB>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
